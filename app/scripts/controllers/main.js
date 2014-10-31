df158e33085d882c3d392141e1db468dece5a408
Marko Kajzer <marko.kajzer@rwth-aachen.de>
2014-10-31 19:40:00 +0100

    Added functionality to the site: Books can be clicked, random placesholders

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,33 @@
 'use strict';
 
-angular.module('bookstoreAngularApp')
-  .controller('BookstoreCtrl', function ($scope) {
-    $scope.books = [{title: 'Book 1', author: 'Author 1'},
-                    {title: 'Book 2', author: 'Author 2'},
-                    {title: 'Book 3', author: 'Author 3'}];
+var app = angular.module('bookstoreAngularApp');
 
+app.controller('BookstoreCtrl', function($http) {
+  var store = this;
+  store.books = [];
+  store.active = 1;
 
-  });
+  $http.get('http://localhost:3000/books')
+    .success(function(data) {
+      store.books = data;
+    });
+    //.error(function(data) {
+    //  store.books = [{title:'Book 1', author:'Author 1', id: "1", isbn: "12345"},
+    //                  {title:'Book 2', author:'Author 2', id: "2", isbn: "67890"},
+    //                  {title:'Book 3', author:'Author 3', id: "3", isbn: "13579"}];
+    //
+    //
+    //});
+
+    this.setActive = function(newActive) {
+      store.active = newActive;
+    };
+
+    this.isActive = function(listElement) {
+      return store.active === listElement;
+    };
+
+    this.addBook = function(newBook) {
+      store.books.push(newBook);
+    };
+});
