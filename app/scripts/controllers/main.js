3c09b4c22383b17a7cff2690a633994612550395
Marko Kajzer <marko.kajzer@rwth-aachen.de>
2014-11-12 14:45:54 +0100

    Added holderjs fix for angular, so placeholders are loaded correctly

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,43 +7,48 @@ app.controller('MainCtrl', function() {
   main.hover = false;
 });
 
-app.controller('BookstoreCtrl', function($http) {
+app.controller('BookstoreCtrl', function($http, $resource) {
+  var Book = $resource('http://localhost:3000/books/:id', {id: '@id'});
+
   var store = this;
-  store.books = [];
+  store.books = Book.query();
   store.newBook = {};
   store.active = 1;
 
-  $http.get('http://localhost:3000/books')
-    .success(function(data) {
-      store.books = data;
-    });
-    //.error(function(data) {
-    //  store.books = [{title:'Book 1', author:'Author 1', id: "1", isbn: "12345"},
-    //                  {title:'Book 2', author:'Author 2', id: "2", isbn: "67890"},
-    //                  {title:'Book 3', author:'Author 3', id: "3", isbn: "13579"}];
-    //
-    //
-    //});
+  this.setActive = function(newActive) {
+    store.active = newActive;
+  };
 
-    this.setActive = function(newActive) {
-      store.active = newActive;
-    };
+  this.isActive = function(listElement) {
+    return store.active === listElement;
+  };
 
-    this.isActive = function(listElement) {
-      return store.active === listElement;
-    };
+  this.addBook = function() {
+    var returnedBook = Book.save(store.newBook);
 
-    this.addBook = function() {
-      store.newBook.id = store.books[store.books.length - 1].id + 1;
-      store.books.push(store.newBook);
-      store.newBook = {};
-    };
+    if(returnedBook['errors']) {
+    }
+    else {
+      store.books.push(returnedBook);
+    }
 
-    this.removeBook = function(book) {
-      // TODO: LETZTES BUCH LÖSCHEN
-      var index = store.books.indexOf(book);
-      store.active = store.books[index + 1].id;
-      store.books.splice(index, 1);
+    //store.newBook.id = store.books[store.books.length - 1].id + 1;
+    store.newBook = {};
+  };
+
+  this.removeBook = function(book) {
+    // TODO: LETZTES BUCH LÖSCHEN
+    var index = store.books.indexOf(book);
+    store.active = store.books[index + 1].id;
+    store.books.splice(index, 1);
+  };
+});
+
+app.directive('holderFix', function () {
+    return {
+        link: function (scope, element, attrs) {
+            Holder.run({ images: element[0], nocss: true });
+        }
     };
 });
 
